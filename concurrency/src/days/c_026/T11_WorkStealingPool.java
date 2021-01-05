eb46c8cd05fd88576c6a6629d062d31a13f98da5
AppleLliao <243261609@qq.com>
2021-01-05 16:57:26 +0800

    1.WorkStealingPool:是工作窃取线程池,2.ForkJoinPool线程池也是精灵线程，内部使用递归 3.多管道并行计算

---
+++ concurrency/src/days/c_026/T11_WorkStealingPool.java
@@ -0,0 +1,44 @@
+package days.c_026;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+//WorkStealingPool:是工作窃取线程池，每一个线程都维护自己的一个队列，
+//每个队列都积累各自的任务，当一个线程的队列任务执行完之后，会主动去别的线程的任务队列中找任务干
+//WorkStealingPool中的线程都是精灵线程（守护线程/后台线程），主线程结束之后，他可能不会结束
+//他的底层是ForkJoinPool
+public class T11_WorkStealingPool {
+    public static void main(String args[]) throws IOException {
+        ExecutorService service = Executors.newWorkStealingPool();
+        System.out.println(Runtime.getRuntime().availableProcessors());
+
+        service.execute(new R(1000));
+        service.execute(new R(2000));
+        service.execute(new R(2000));
+        service.execute(new R(2000));
+        service.execute(new R(2000));
+
+        //由于产生的精灵线程（守护线程，后台线程），主线程不阻塞的话，看不到输出
+        System.in.read();
+    }
+
+
+    static class R implements  Runnable{
+        int time;
+
+        R(int t){
+            this.time=t;
+        }
+        @Override
+        public void run() {
+            try {
+                TimeUnit.MILLISECONDS.sleep(time);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(time+""+Thread.currentThread().getName());
+        }
+    }
+}
---
+++ concurrency/src/days/c_026/T14ParallelStreamApi.java
@@ -0,0 +1,42 @@
+package days.c_026;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class T14ParallelStreamApi {
+    public static void main(String[] args){
+        List<Integer> nums =new ArrayList<>();
+        Random r=new Random();
+        for(int i=0;i<10000;i++){
+            nums.add(1000000+r.nextInt(1000000));
+        }
+
+        long start =System.currentTimeMillis();
+        nums.forEach(v->isPrime(v));
+        long end=System.currentTimeMillis();
+        System.out.println(end-start); //2126
+
+        //使用parallel stream api
+
+        start=System.currentTimeMillis();
+        nums.stream().forEach(T14ParallelStreamApi::isPrime); //单管道
+        end=System.currentTimeMillis();
+        System.out.println(end-start); //2111
+
+        start=System.currentTimeMillis();
+        nums.parallelStream().forEach(T14ParallelStreamApi::isPrime); //多管道
+        end=System.currentTimeMillis();
+        System.out.println(end-start); //390
+    }
+
+    //判断一个数是不是质数
+    private static boolean isPrime(Integer num){
+        for(int i=2;i<num/2;i++){
+            if(num%i==0){
+                return false;
+            }
+        }
+        return  true;
+    }
+}
